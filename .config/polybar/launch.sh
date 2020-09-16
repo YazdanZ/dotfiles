a6a357ce0abd2c5efd635199b017740e5b24ad0b
Yazdan Zinati <yazdanzin@gmail.com>
2020-09-16 18:37:50 -0400

    Add a new bar at the botttom for mpd controls

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,5 +8,7 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar topbar >>/tmp/polybar1.log 2>&1 & disown
+polybar bottombar >>/tmp/polybar2.log 2>&1 & disown
+
 
 echo "Bars launched..."
